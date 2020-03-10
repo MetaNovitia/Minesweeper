27af598172631057fcbf4dd09f1581347f6386e0
Meta Novitia <mnovitia@uci.edu>
2020-03-09 21:41:14 -0700

    Updates

---
+++ precache-manifest.4e76aa8380995b91507094bef819879d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9662a63352a9d97d244bc8e863701fb9",
+    "url": "/Minesweeper/index.html"
+  },
+  {
+    "revision": "28943f1d52554784bd3b",
+    "url": "/Minesweeper/static/css/main.2afeccc6.chunk.css"
+  },
+  {
+    "revision": "8e99d8e747d9eea5bc75",
+    "url": "/Minesweeper/static/js/2.92fecf48.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Minesweeper/static/js/2.92fecf48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28943f1d52554784bd3b",
+    "url": "/Minesweeper/static/js/main.28cf0fe9.chunk.js"
+  },
+  {
+    "revision": "bd242014ca7ad1b7ddca",
+    "url": "/Minesweeper/static/js/runtime-main.4eff02b5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Minesweeper/precache-manifest.eb63c2c0e2fa8a29cabeca3a9e792d5b.js"
+  "/Minesweeper/precache-manifest.4e76aa8380995b91507094bef819879d.js"
 );
 
 self.addEventListener('message', (event) => {
